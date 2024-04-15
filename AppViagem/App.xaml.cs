da323fa587028f9269fbbf4a76b5e462e64b2e21
Usuario <Usuario@CPD03-M13>
2024-04-15 11:14:30 -0300

    Adicionar arquivos de projeto.

---
+++ AppViagem/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppViagem
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppViagem/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppViagem
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
