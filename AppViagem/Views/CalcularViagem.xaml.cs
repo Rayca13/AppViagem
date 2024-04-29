9430706f014799cf3e37e6b00b27dc5568015a16
Usuario <Usuario@CPD03-M12>
2024-04-29 11:48:42 -0300

    config paginas maui

--- AppViagem/Views/CalcularViagem.xaml.cs
+++ AppViagem/Views/CalcularViagem.xaml.cs
@@ -1,9 +1,21 @@
+using AppViagem.Models;
+
 namespace AppViagem.Views;
 
 public partial class CalcularViagem : ContentPage
 {
-	public CalcularViagem()
-	{
-		InitializeComponent();
-	}
+    public CalcularViagem()
+    {
+        InitializeComponent();
+    }
+
+    private void btn_novaViagem_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btn_somar_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
--- AppViagem/Views/CriarPedagio.xaml.cs
+++ AppViagem/Views/CriarPedagio.xaml.cs
@@ -1,3 +1,5 @@
+using AppViagem.Models;
+
 namespace AppViagem.Views;
 
 public partial class CriarPedagio : ContentPage
@@ -6,4 +8,21 @@ public partial class CriarPedagio : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void btn_enviar_Clicked(object sender, EventArgs e)
+    {
+		try 
+		{
+			Pedagio p = new Pedagio();
+			p.local = txt_local.Text;
+			p.valor = Convert.ToDouble(txt_valor.Text);
+
+			await App.Db.Insert(p);
+			await DisplayAlert("Sucesso!", "Pedagio foi adicionado", "Ok");
+		}
+		catch (Exception ex) 
+		{
+		
+		}
+    }
 }
